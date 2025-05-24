5ee43dcd11af7b73afab0d569db6758b4f2b3e94
='Surya <='suryasaud2002@gmail.com>
2025-05-24 11:31:42 +0545

    change code

--- surya.php
+++ surya.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    Hi! I am using Vs code
+<>
+    <h1>Welcome to php</h1>
+    <p>Today we are studying about php in details</p>
 </body>
 </html>
